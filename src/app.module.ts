558f47e65a7e05bc2479750301b6a9635daf06ab
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-11 17:26:37 +0100

    Resource Common

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot()],
+  imports: [MoviesModule, ConfigModule.forRoot(), CommonModule],
 })
 export class AppModule {}
