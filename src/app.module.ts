bdf59df546f93f06cdb7c4b5bf0181228f623ccf
SCelso <sergio.celso.sanchez@gmail.com>
2022-11-11 18:12:26 +0100

    Movies Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,20 @@ import { CommonModule } from './common/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    MoviesModule,
-    CommonModule,
-  ],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+        MoviesModule,
+        CommonModule,
+    ],
 })
 export class AppModule {}
--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,72 @@
-export class Movie {}
+import {
+    BeforeInsert,
+    BeforeUpdate,
+    Column,
+    Entity,
+    PrimaryColumn,
+} from 'typeorm';
+
+@Entity()
+export class Movie {
+    @PrimaryColumn('uuid')
+    id: string;
+    @Column('text', {
+        unique: true,
+    })
+    name: string;
+
+    @Column('text')
+    synopsis: string;
+
+    @Column()
+    releaseDate: number;
+    @Column('text', {
+        unique: true,
+    })
+    slug: string;
+
+    @Column('text', {
+        array: true,
+    })
+    genre: string[];
+
+    @Column('int', {
+        default: 0,
+    })
+    rating: number;
+
+    @Column('text')
+    director: string;
+
+    @Column('text')
+    mainActor: string;
+
+    @Column('int', {
+        default: 0,
+    })
+    duration: number;
+
+    @BeforeInsert()
+    checkSlugInsert() {
+        if (!this.slug) {
+            this.slug = this.name;
+        }
+
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '');
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
+        if (!this.slug) {
+            this.slug = this.name;
+        }
+
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '');
+    }
+}
