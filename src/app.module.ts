fd5601ab9c9c0d383a0a3e07801d1e1a43a71a39
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-11 17:31:26 +0100

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot(), CommonModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MoviesModule,
+    CommonModule,
+  ],
 })
 export class AppModule {}
