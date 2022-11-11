70a17463d49a97d0204f377671cd821f5adce558
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-11 17:32:34 +0100

    Set GlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  await app.listen(3000);
 }
 
 bootstrap();
