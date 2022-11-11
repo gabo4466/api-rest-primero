8b826b0498b84124b14ef64af9db4f464c0c4b1c
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-11 17:18:27 +0100

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule],
-  
+  imports: [MoviesModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
